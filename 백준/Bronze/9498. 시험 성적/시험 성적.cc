5678375e6a4be1927d99c016c7a3ef0cfdd7ddd0
Seoyeon <102731509+Seoyeonni@users.noreply.github.com>
2023-03-10 01:50:27 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	int grade;
+
+	scanf("%d", &grade);
+
+	if (grade >= 90 && grade <= 100) {
+		printf("A\n");
+	}
+	else if (grade >= 80 && grade < 90) {
+		printf("B\n");
+	}
+	else if (grade >= 70 && grade < 80) {
+		printf("C\n");
+	}
+	else if (grade >= 60 && grade < 70) {
+		printf("D\n");
+	}
+	else {
+		printf("F\n");
+	}
+
+	return 0;
+}
