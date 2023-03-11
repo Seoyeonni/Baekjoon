f947058b50145e49ac64466f6bbedad6371b1a6f
Seoyeon <102731509+Seoyeonni@users.noreply.github.com>
2023-03-12 03:26:19 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	if (n % 4 == 0) {
+		if (n % 100 != 0) {
+			printf("1\n");
+		}
+		else if (n % 400 == 0) {
+			printf("1\n");
+		}
+		else {
+			printf("0\n");
+		}
+	}
+	else {
+		printf("0\n");
+	}
+
+	return 0;
+}
