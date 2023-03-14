a3238b720521a098ae302c7433549525749d4d41
Seoyeon <102731509+Seoyeonni@users.noreply.github.com>
2023-03-14 14:19:58 +0900

    Update 숫자 카드.cc

--- 백준/Silver/10815. 숫자 카드/숫자 카드.cc
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -5,27 +5,6 @@ using namespace std;
 int n_arr[500001];
 int m_arr[500001];
 
-bool binary_search(int n, int k)
-{
-	int start = 0, end = n - 1, mid;
-
-	while (start <= end) {
-		mid = (start + end) / 2;
-
-		if (n_arr[mid] == k) {
-			return 1;
-		}
-		else if (n_arr[mid] > k) {
-			end = mid - 1;
-		}
-		else {
-			start = mid + 1;
-		}
-	}
-
-	return 0;
-}
-
 int main()
 {
 	int n, m, i;
