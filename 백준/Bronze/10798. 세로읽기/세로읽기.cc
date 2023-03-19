39912652ec1a61b0ed18b1df70e9dfa84c250343
Seoyeon <102731509+Seoyeonni@users.noreply.github.com>
2023-03-19 17:14:15 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	string str[5];
+
+	for (int i = 0; i < 5; i++) {
+		cin >> str[i];
+	}
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (i < str[j].size()) {
+				cout << str[j][i];
+			}
+		}
+	}
+
+	cout << endl;
+}
