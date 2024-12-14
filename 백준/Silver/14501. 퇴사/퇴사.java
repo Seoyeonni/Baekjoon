74a835989819dd8ef1b4de8d5ff11b3d05feb4af
Seoyeon <102731509+Seoyeonni@users.noreply.github.com>
2024-12-14 21:19:32 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,46 +4,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n;
-	static int[][] schedule;
-	static int profit = 0;
+	static int N;
+	static int[] T;
+	static int[] P;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		n = Integer.parseInt(br.readLine());
+		N = Integer.parseInt(br.readLine());
 
 		StringTokenizer st;
 
-		schedule = new int[n][2];
-		for (int i = 0; i < n; i++) {
+		T = new int[N + 1];
+		P = new int[N + 1];
+		
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			schedule[i][0] = Integer.parseInt(st.nextToken());
-			schedule[i][1] = Integer.parseInt(st.nextToken());
+			T[i] = Integer.parseInt(st.nextToken());
+			P[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		dfs(0, 0);
-		
-		System.out.println(profit);
+		System.out.println(maxProfit());
 	} // end of main()
 	
-	static void dfs(int i, int pay) {
-		if (i >= n) {
-			profit = Math.max(pay, profit);
-			return;
+	static int maxProfit() {
+		int[] DP = new int[N + 2];
+		
+		for (int i = N; i > 0; i--) {
+			// 상담을 했을 때 다음 상담 가능 날짜
+			int next = i + T[i];
+			
+			// 상담을 할 수 있다면
+			if (next <= N + 1) {
+				DP[i] = Math.max(DP[i + 1], P[i] + DP[next]);
+			}
+			// 상담을 할 수 없다면
+			else {
+				DP[i] = DP[i + 1];
+			}
 		}
 		
-		// 다음 상담을 마칠 수 있다면
-		if (i + schedule[i][0] <= n) {
-			dfs(i + schedule[i][0], pay + schedule[i][1]);
-		}
-		// 다음 상담을 마칠 수 없다면
-		else {
-			// 탈출 조건
-			dfs(i + schedule[i][0], pay);
-		}
-		
-		// 다른 경우의 수 탐색
-		dfs(i + 1, pay);
+		return DP[1];
 	} // end of dfs()
 } // end of Main
