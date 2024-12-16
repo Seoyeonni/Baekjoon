8f7f96a64b53f0062ed16a6cc2fd314c5ed5fa69
Seoyeon <102731509+Seoyeonni@users.noreply.github.com>
2024-12-16 17:34:21 +0900

    [Silver I] Title: 스타트와 링크, Time: 268 ms, Memory: 20592 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -7,7 +7,7 @@ public class Main {
 	static int N;
 	static int[][] S;
 	static int min = Integer.MAX_VALUE;
-	static boolean[] chk;
+	static boolean[] check;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,24 +24,24 @@ public class Main {
 			}
 		}
 		
-		chk = new boolean[N + 1];
-		chk[1] = true;
-		chkTeam(1, 1);
+		check = new boolean[N + 1];
+		check[1] = true;
+		dfs(1, 1);
 		
 		System.out.println(min);
 	} // end of main()
 	
-	static void chkTeam(int count, int before) {
+	static void dfs(int count, int before) {
 		if (count == N / 2) {
 			calcMin();
 			return;
 		}
 		
-		for (int i = 2; i <= N; i++) {
-			if (!chk[i] && i > before) {
-				chk[i] = true;
-				chkTeam(count + 1, i);
-				chk[i] = false;
+		for (int i = before + 1; i <= N; i++) {
+			if (!check[i]) {
+				check[i] = true;
+				dfs(count + 1, i);
+				check[i] = false;
 			}
 		}
 	}
@@ -50,17 +50,25 @@ public class Main {
 		int sum1 = 0;
 		int sum2 = 0;
 		
-		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= N; j++) {
-				if (chk[i] && chk[j]) {
+		for (int i = 1; i < N; i++) {
+			for (int j = i + 1; j <= N; j++) {
+				if (check[i] && check[j]) {
 					sum1 += S[i][j];
+					sum1 += S[j][i];
 				}
-				else if (!chk[i] && !chk[j]){
+				else if (!check[i] && !check[j]){
 					sum2 += S[i][j];
+					sum2 += S[j][i];
 				}
 			}
 		}
 		
 		min = Math.min(min, Math.abs(sum1 - sum2));
+		
+		// 현재 최솟값이 0이라면 더 이상 탐색할 필요 없음
+		if (min == 0) {
+			System.out.println(min);
+			System.exit(0);
+		}
 	}
 } // end of Main
